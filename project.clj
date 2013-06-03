34b4ded7663b7ea04019127754e394d71530f4fe
Stuart Sierra <mail@stuartsierra.com>
2013-06-03 10:22:26 -0400

    Add description/url/license to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject com.stuartsierra/dependency "0.1.1"
+  :description "A data structure for representing dependency graphs"
+  :url "https://github.com/stuartsierra/dependency"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :clj-1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :clj-1.3.0 {:dependencies [[org.clojure/clojure "1.3.0"]]}})
