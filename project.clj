930d28d0bb2cd8a84356910d3e68c400e6c86ad7
Stuart Sierra <mail@stuartsierra.com>
2015-09-18 11:39:08 -0400

    Set development version to 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.stuartsierra/dependency "0.2.0"
+(defproject com.stuartsierra/dependency "0.2.1-SNAPSHOT"
   :description "A data structure for representing dependency graphs"
   :url "https://github.com/stuartsierra/dependency"
   :license {:name "Eclipse Public License"
