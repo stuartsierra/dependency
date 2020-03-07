db912faf2020453e68e8533df8743b30466f2303
Stuart Sierra <mail@stuartsierra.com>
2020-03-07 11:24:53 -0500

    Set Clojure as provided-scope dependency
    This prevents a specific Clojure version from being loaded as a
    transitive dependency of this library.
    
    See also https://github.com/stuartsierra/component/issues/65

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url "https://github.com/stuartsierra/dependency"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0-alpha5"]]}
-             :clj-1.7.0 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
+  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]])
